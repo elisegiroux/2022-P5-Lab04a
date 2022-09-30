68c8e5bf3aec2e57d3be17c3185b6d002d8d2129
Elise Giroux <elise.giroux@vtsd.com>
2022-09-30 13:09:08 -0400

    9/30 Lab04 - 90 Point Version

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,21 +7,32 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
+
         System.out.println("Lab04a, Student Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double numMonths = numYears * 12;
+
+        double monthlyrate = (annualRate/100) / 12;
+        double numerator = monthlyrate * Math.pow(1+ monthlyrate, numMonths);
+        double denominator = Math.pow(1+ monthlyrate, numMonths) - 1;
+        double monthlyPayment = (numerator / denominator) * principal;
+        double totalpayments = monthlyPayment * numMonths;
+        double totalinterest = totalpayments - principal;
 
 
 
 
 
-
-
-
-
-        System.out.println();
+        System.out.println("Principle:         $"+principal);
+        System.out.println("Annual Rate:       "+annualRate + "%" );
+        System.out.println("Number of Years:   "+numYears);
+        System.out.println("Monthly Payment:   $"+ monthlyPayment);
+        System.out.println("Total Payments:    $" + totalpayments);
+        System.out.println("Total Interest:    $" + totalinterest);
     }
+
 }
 
